eec3bd8093732f1e250ae243017bd68e6f973cc5
Juan Alberto <japcreg@gmail.com>
2017-08-03 23:58:03 +0200

    Added some voice

--- Shadow.js
+++ Shadow.js
@@ -3,19 +3,24 @@ const client = new Discord.Client();
 var Config = require('./config.json');
 
 // Command objects
-var Cb = require('cleverbot-node');
-var cleverbot = new Cb()
-Cb.prepare(function () {
-  console.log('Cleverbot iniciado')
-});
 var fs = require('fs');
 
-// Ark-list command / TODO Move to own file
-var arkTasksList = [];
-
-client.on('ready', () => {
-	arkListLoad();
+// Media wiki
+var wtf_wikipedia = require("wtf_wikipedia")
+var mwbot = require('nodemw');
+var mwclient = new mwbot({
+    protocol: 'https',           // Wikipedia now enforces HTTPS
+    server: 'es.wikipedia.org',  // host name of MediaWiki-powered site
+    path: '/w',                  // path to api.php script
+    debug: false                 // is more verbose when set to true
 });
+var iclient = new mwbot({
+    protocol: 'http',           // Wikipedia now enforces HTTPS
+    server: 'inciclopedia.wikia.com',  // host name of MediaWiki-powered site
+    path: '/w',                  // path to api.php script
+    debug: false                 // is more verbose when set to true
+});
+
 
 client.on('message', msg => {
 
@@ -27,6 +32,16 @@ client.on('message', msg => {
   		console.log(msg.author.username + ' used \'ping\'');
    		msg.reply('pong');
   	}
+
+    if(msg.content.startsWith(prefix + 'say')){
+        console.log(msg.author.username + ' used \'say\'');
+        var message = msg.content.slice(prefix.length + 'say'.length,msg.content.length);
+        if(message.length <= 0){
+            msg.reply("I can't say nothing :3");
+        }else{
+            msg.channel.send(message, {tts:true});
+        }   
+    }
   	
   	if(msg.content.startsWith(prefix + 'randomcat')){
   		console.log(msg.author.username + ' used \'randomcat\'')
@@ -36,151 +51,120 @@ client.on('message', msg => {
 	        try {
 	          JSON.parse(body)
 	        } catch (e) {
-	        	msg.channel.sendMessage('Mala respuesta desde la api')
+	        	msg.channel.send('Mala respuesta desde la api')
 	          return
 	        }
 	        var cat = JSON.parse(body)
-	        msg.channel.sendFile(cat.file)
+	        msg.channel.send(cat.file)
 	      }else{
-	      	msg.channel.sendMessage('Ups algo ha ido mal, vuelve a intentarlo');
+	      	msg.channel.send('Ups algo ha ido mal, vuelve a intentarlo');
 	      }
 	    })
     }
-    
-    if(msg.content.startsWith(prefix + 'cleverbot')){
-  		console.log(msg.author.username + ' used \'cleverbot\'');
-  		let suffix = msg.content.split(" ").slice(1);
-  		if((suffix !== undefined) && (suffix.length > 0)){		
-			msg.channel.startTyping()
-		    cleverbot.write(suffix, function (r) {
-		      msg.channel.sendMessage(r.message);
-		      msg.channel.stopTyping()
-		    })
-		}
-    }
-    
-    if(msg.content.startsWith(prefix + 'arklist')){
-    	console.log(msg.author.username + ' used \'arklist\'');
-    	let suffix = msg.content.split(" ");
-    	let info = "";
-    	if(suffix.length > 1)
-    	{
-    		switch(suffix[1])
-    		{
-    			case "add":
-    				info = arkListAdd(msg.content.split("add").slice(1));
-    				break;
-    			case "remove":
-    				info = arkListRemove(suffix[2]);
-    				break;
-    			case "show":
-    				info = arkListShow();
-    				break;
-    			case "assign":
-    				info = arkListAssign(suffix[2],suffix[3]);
-    				break;
-    			case "save":
-    				info = arkListSave();
-    				break;
-    			case "help":
-    				info = "Use: !arklist add task || !arklist remove taskId || !arklist show || !arklist assign task player."
-    				break;
-    		}
-    	}
-    	else
-    	{
-    		info = "Incorrect format. Use: !arklist add task || !arklist show || !arklist assign task player.";
-    	}
 
-    	msg.channel.sendMessage(info);
+    if(msg.content.startsWith(prefix + 'randomdog')){
+        console.log(msg.author.username + ' used \'randomdog\'')
+        var request = require('request')
+        request('http://thedogapi.co.uk/api/v1/dog?limit=1', function (error, response, body) {
+          if (!error && response.statusCode === 200) {
+            try {
+              JSON.parse(body);
+            } catch (e) {
+                msg.channel.send('Mala respuesta desde la api')
+              return
+            }
+            var dog = JSON.parse(body);
+            msg.channel.send(dog.data[0].url);
+          }else{
+            msg.channel.send('Ups algo ha ido mal, vuelve a intentarlo');
+          }
+        })
     }
+
+    if(msg.content.startsWith(prefix + 'yesno')){
+        console.log(msg.author.username + ' used \'yesno\'')
+        var request = require('request')
+        request('https://yesno.wtf/api/', function (error, response, body) {
+          if (!error && response.statusCode === 200) {
+            try {
+              JSON.parse(body);
+            } catch (e) {
+                msg.channel.send('Mala respuesta desde la api')
+              return
+            }
+            var yesno = JSON.parse(body);
+            msg.channel.send(yesno.image);
+          }else{
+            msg.channel.send('Ups algo ha ido mal, vuelve a intentarlo');
+          }
+        })
+    }
+
+    if(msg.content.startsWith(prefix + 'yomomma')){
+        console.log(msg.author.username + ' used \'yomomma\'')
+        var request = require('request')
+        request('http://api.yomomma.info/', function (error, response, body) {
+          if (!error && response.statusCode === 200) {
+            try {
+              JSON.parse(body);
+            } catch (e) {
+                msg.channel.send('Mala respuesta desde la api');
+              return
+            }
+            var yomomma = JSON.parse(body);
+            msg.channel.send(yomomma.joke);
+          }else{
+            msg.channel.send('Ups algo ha ido mal, vuelve a intentarlo');
+          }
+        })
+    }
+
+    if(msg.content.startsWith(prefix + 'wiki')){
+        console.log(msg.author.username + ' used \'wiki\'')
+
+        var suffix = match[1];
+        console.log("------------- STARTING QUERY -------------")
+        mwclient.getArticle(suffix, undefined, function(err , info, next){
+            if(err != null){
+                console.log('There was an error --> ' + err);
+            }else{
+                wikinfo = wtf_wikipedia.parse(info);
+                console.log(wikinfo);
+                if(wikinfo.text !== undefined && wikinfo.text.get("Intro") !== undefined){
+                    msg.channel.send(wikinfo.text.get("Intro")[0].text);
+                }
+            }
+        })
+        console.log("------------- FINISHING QUERY -------------");
+    }
+
+    // Voice things
+
+    if (msg.content.startsWith(prefix + 'join')) {
+        console.log(msg.author.username + ' used \'join\'');
+        if (!msg.guild) return;
+
+        if (msg.member.voiceChannel) {
+          msg.member.voiceChannel.join()
+            .then(connection => {
+              msg.reply('I have successfully connected to the channel!');
+              var dispatcher = connection.playFile("test.mp3");
+            })
+            .catch(console.log);
+        } else {
+          msg.reply('You need to join a voice channel first!');
+        }
+    }
+
+    if (msg.content.startsWith(prefix + 'leave')) {
+        console.log(msg.author.username + ' used \'leave\'');
+        if (!msg.guild) return;
+
+        if (msg.member.voiceChannel) {
+          msg.member.voiceChannel.leave();
+        }
+    }
+
 });
-
-function arkListAdd(task){
-	let result = "";
-	var playerTask ={};
-
-	playerTask["task"] = task;
-	playerTask["player"] = "Unasigned";
-	arkTasksList.push(playerTask);
-
-	result = "Added";
-
-	return result;
-}
-
-function arkListRemove(task){
-	let result = "";
-	task = parseInt(task);
-	if(task >= 0 && task < arkTasksList.length ){
-		console.log("Remove " + task);
-		arkTasksList.splice(task, 1);
-		result = "Removed";
-	}else{
-		result = "Task id not valid";
-	}
-
-	return result;
-}
-
-function arkListShow(){
-	let result = "";
-
-	if(arkTasksList.length == 0)
-	{
-		result = "The list is empty";
-	}else
-	{
-		var msgArray  = [];
-		msgArray.push('```ini');
-
-		for(i=0; i < arkTasksList.length; i++)
-		{
-			msgArray.push(i + ")" + arkTasksList[i]["task"] + " - " + arkTasksList[i]["player"] + "\n");
-		}
-
-		msgArray.push('```');
-		result += msgArray.join('\n');
-	}
-
-	return result;
-}
-
-function arkListAssign(player, task){
-	let result = "";
-
-	arkTasksList[task]["player"] = player;
-	result = "Assigned";
-
-	return result;
-}
-
-function arkListSave(){
-	let result = "Changes saved";
-	let jsonData = JSON.stringify(arkTasksList);
-
-	fs.writeFile("arkTasksList.json", jsonData, function(err) {
-	    if(err) {
-	    	result = "There was an error";
-	        return console.log(err);
-	    }
-	});
-
-	return result;
-}
-
-function arkListLoad(){
-	console.log('Loading arkTasksList');
-
-  	fs.readFile('arkTasksList.json', 'utf8', function(err, data) {  
-    	if (err != null && err.code === "ENOENT"){
-    		console.log('There was a problem loading file arkTasksList.json');
-    	} else{
-    		arkTasksList = JSON.parse(data);
-    	}
-  	});
-
-  	console.log('Done');	
-}
 
 client.login(Config.shadow.token)
