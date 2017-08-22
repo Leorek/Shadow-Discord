c53855edc2e3a5f81bbacea75a2b91e47d91ae26
Juan Alberto <japc93k@gmail.com>
2017-08-22 22:57:00 +0200

    Added more refactor

---
+++ Core/Commands/Fun.js
@@ -0,0 +1,103 @@
+var Commands = [];
+var fs = require('fs');
+var Request = require('request');
+
+Commands.say = {
+  name: 'say',
+  help: "Repeats with tts!",
+  fn: function (msg, suffix) {
+    if(suffix.length <= 0){
+      msg.reply("I can't say nothing :3");
+    }else{
+      msg.channel.send(message, {tts:true});
+    }   
+  }
+}
+
+Commands.randomcat = {
+  name: 'randomcat',
+  help: "Sends a random picture or gif of cats.",
+  fn: function (msg, suffix) {
+    Request('http://random.cat/meow', function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        try {
+          JSON.parse(body)
+        } catch (e) {
+          msg.channel.send('Mala respuesta desde la api')
+          return
+        }
+        var cat = JSON.parse(body)
+        msg.channel.send(cat.file)
+      }else{
+        msg.channel.send('Ups algo ha ido mal, vuelve a intentarlo');
+      }
+    })
+  }
+}
+
+Commands.randomdog = {
+  name: 'randomdog',
+  help: "Sends a random picture or gif of doggies.",
+  fn: function (msg, suffix) {
+    Request('http://thedogapi.co.uk/api/v1/dog?limit=1', function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        try {
+          JSON.parse(body);
+        } catch (e) {
+          msg.channel.send('Mala respuesta desde la api')
+          return
+        }
+        var dog = JSON.parse(body);
+          msg.channel.send(dog.data[0].url);
+      }else{
+        msg.channel.send('Ups algo ha ido mal, vuelve a intentarlo');
+      }
+    })
+  }
+}
+
+
+Commands.yesno = {
+  name: 'yesno',
+  help: "Sends a random gif or picture with yes or no",
+  fn: function (msg, suffix) {
+    Request('https://yesno.wtf/api/', function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        try {
+          JSON.parse(body);
+        } catch (e) {
+          msg.channel.send('Mala respuesta desde la api')
+          return
+        }
+        var yesno = JSON.parse(body);
+        msg.channel.send(yesno.image);
+      }else{
+        msg.channel.send('Ups algo ha ido mal, vuelve a intentarlo');
+      }
+    })
+  }
+}
+
+
+Commands.yomomma = {
+  name: 'yomomma',
+  help: "Sends a random joke about ur mum.",
+  fn: function (msg, suffix) {
+      Request('http://api.yomomma.info/', function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        try {
+          JSON.parse(body);
+        } catch (e) {
+          msg.channel.send('Mala respuesta desde la api');
+          return
+        }
+        var yomomma = JSON.parse(body);
+        msg.channel.send(yomomma.joke);
+      }else{
+        msg.channel.send('Ups algo ha ido mal, vuelve a intentarlo');
+      }
+    })
+  }
+}
+
+exports.Commands = Commands
