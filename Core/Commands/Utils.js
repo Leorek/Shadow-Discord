f6a7af682397c2490eae18cdfe855cab3e4f1e28
Juan Alberto <japc93k@gmail.com>
2017-08-30 11:25:14 +0200

    Fixed typo

--- Core/Commands/Utils.js
+++ Core/Commands/Utils.js
@@ -4,7 +4,7 @@ Commands.avatar = {
   name: 'avatar',
   help: "I'll reply with a link to your avatar!",
   fn: function (msg) {
-    msg.reply("This is your avatar: " + message.author.avatarURL);
+    msg.reply("This is your avatar: " + msg.author.avatarURL);
   }
 }
 
