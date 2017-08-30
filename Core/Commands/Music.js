9396fe1787abae8dea9f476436185c897eb4e6f1
Juan Alberto <japc93k@gmail.com>
2017-08-30 21:19:49 +0200

    Added support for multiserver on music commands, also added queue command.

--- Core/Commands/Music.js
+++ Core/Commands/Music.js
@@ -1,13 +1,16 @@
 var ytdl = require('ytdl-core');
 var Request = require('request');
 var Config = require('../../config.json');
-var voice_connection = null;
-var voice_handler = null;
-var DEFAULT_VOLUME = 100;
+
+// Handlers
+var channelToSendInfo = [];
+var voiceConnection = [];
+var musicStream = [];
+
+var actualVolume = [];
+var isPaused = [];
 var queue = [];
-var now_playing_data = {};
-var inform_np = true;
-var paused = false;
+
 var Commands = [];
 
 Commands.join = {
@@ -15,13 +18,16 @@ Commands.join = {
   help: "Makes Shadow to join a voice channel.",
   fn: function (msg, suffix) {
     if (!msg.guild) return;
-    text_channel = msg.guild.channels.find(chn => chn.name === 'general' && chn.type === "text");
-    if(text_channel === null) throw "Couldn't find text channel '#" + 'general' + "' in server '" + server_name + "'";
+    channelToSendInfo[msg.guild.id] = msg.guild.channels.find(chn => chn.name === 'general' && chn.type === "text");
+    if(channelToSendInfo[msg.guild.id] === null) throw "Couldn't find text channel '#" + 'general' + "' in server.";
     if (msg.member.voiceChannel) {
       msg.member.voiceChannel.join()
         .then(connection => {
           msg.reply('I have successfully connected to the channel!');
-          voice_connection = connection;
+          voiceConnection[msg.guild.id] = connection;
+          queue[msg.guild.id] = [];
+          actualVolume[msg.guild.id] = 100;
+          isPaused[msg.guild.id] = true;
         })
         .catch(console.log);
     } else {
@@ -32,11 +38,12 @@ Commands.join = {
 
 Commands.leave = {
   name: 'leave',
-  help: "Makes Shadow to .leave actual voice channel.",
+  help: "Makes Shadow to leave actual voice channel.",
   fn: function (msg, suffix) {
-    if(voice_connection !== null) {
-      msg.reply("Leaving channel.");
-      voice_connection.channel.leave();
+    if(voiceConnection[msg.guild.id] !== null) {
+      msg.reply("Leaving voice channel.");
+      voiceConnection[msg.guild.id].channel.leave();
+      queue[msg.guild.id] = undefined;
     } else {
       msg.reply("I'm not in a voice channel.");
     }
@@ -47,8 +54,8 @@ Commands.request = {
   name: 'request',
   help: "Searchs a song on youtube and adds it to the queue.",
   fn: function (msg, suffix) {
-    if(voice_connection !== null) {
-      search_video(msg, suffix);
+    if(voiceConnection[msg.guild.id] !== null) {
+      searchVideo(msg, suffix);
     }else{
       msg.reply("I must be in a voice channel to do that.")
     }
@@ -59,14 +66,14 @@ Commands.volume = {
   name: 'volume',
   help: "Sets volume or returns actual one if no suffix specified.",
   fn: function (msg, suffix, bot) {
-    new_volume = parseInt(suffix);
-    if(Number.isInteger(new_volume)){
-      DEFAULT_VOLUME = new_volume;
-      if(voice_handler !== null){
-        voice_handler.setVolume(DEFAULT_VOLUME/100);
+    var newVolume = parseInt(suffix);
+    if(Number.isInteger(newVolume)){
+      actualVolume[msg.guild.id] = newVolume;
+      if(isBotPlaying(msg.guild.id)){
+        musicStream[msg.guild.id].setVolume(actualVolume[msg.guild.id]/100);
       }
     }
-    msg.reply("Volume: " + DEFAULT_VOLUME + "%");
+    msg.reply("Volume: " + actualVolume[msg.guild.id] + "%");
   }
 }
 
@@ -74,8 +81,13 @@ Commands.pause = {
   name: 'pause',
   help: "Pauses the actual song.",
   fn: function (msg, suffix) {
-    paused = true;
-    voice_handler.pause();
+    if(!isPaused[msg.guild.id]){
+      msg.reply("Pausing player.");
+      isPaused[msg.guild.id] = true;
+      musicStream[msg.guild.id].pause();
+    }else{
+      msg.reply("The player is already paused.");
+    }
   }
 }
 
@@ -83,8 +95,13 @@ Commands.resume = {
   name: 'resume',
   help: "Resumes the actual song.",
   fn: function (msg, suffix) {
-    paused = true;
-    voice_handler.pause();
+    if(isPaused[msg.guild.id]){
+      msg.reply("Resuming player.");
+      isPaused[msg.guild.id] = false;
+      musicStream[msg.guild.id].resume();
+    }else{
+      msg.reply("The player is not paused.");
+    }
   }
 }
 
@@ -92,50 +109,91 @@ Commands.skip = {
   name: 'skip',
   help: "Skips the actual song.",
   fn: function (msg, suffix) {
-    if(voice_handler !== null) {
-      msg.reply("Skipping...");
-      voice_handler.end();
-    } else {
-      msg.reply("There is nothing being played.");
+    if(voiceConnection[msg.guild.id] ){
+      if(isBotPlaying(msg.guild.id)) {
+        msg.reply("Skipping...");
+        musicStream[msg.guild.id].end();
+      } else {
+        msg.reply("There is nothing being played.");
+      }        
+    }else{
+      msg.reply("I'm not in a voice channel.");
     }
   }
 }
 
-function search_video(message, query) {
+Commands.queue = {
+  name: 'queue',
+  help: "Shows the actual queue.",
+  fn: function (msg, suffix) {
+    if(queue[msg.guild.id] !== undefined){
+      if(!isQueueEmpty(msg.guild.id)){
+        var queueItems = [];
+        for(var i=0; i < queue[msg.guild.id].length; i++){
+          queueItems.push("\"" + queue[msg.guild.id][i].title + "\" requested by " + queue[msg.guild.id][i].user);
+        }
+
+        msg.channel.send(
+          {
+            "embed": {
+              "color": 2645853,
+              "author": {
+                "name": "Shadow's queue",
+                "icon_url": "http://icons.iconarchive.com/icons/dtafalonso/yosemite-flat/512/Music-icon.png"
+              },
+              "fields": [
+                {
+                  "name": "Items",
+                  "value": queueItems.join(" \n ")
+                }
+              ]
+            }
+          }
+        );
+      }else{
+        msg.reply("The queue is empty.");
+      }
+    }else{
+      msg.reply("I'm not connected to a voice channel.");
+    }
+  }
+}
+
+
+function searchVideo(msg, query) {
   
   Request("https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=" + encodeURIComponent(query) + "&key=" + Config.api_keys.youtube, (error, response, body) => {
     var json = JSON.parse(body);
     if("error" in json) {
-      message.reply("An error has occurred: " + json.error.errors[0].message + " - " + json.error.errors[0].reason);
+      msg.reply("An error has occurred: " + json.error.errors[0].msg + " - " + json.error.errors[0].reason);
     } else if(json.items.length === 0) {
-      message.reply("No videos found matching the search criteria.");
+      msg.reply("No videos found matching the search criteria.");
     } else {
-      add_to_queue(json.items[0].id.videoId, message);
+      addToQueue(json.items[0].id.videoId, msg);
     }
   })
 }
 
-function add_to_queue(video, message, mute = false) {
+function addToQueue(video, msg, mute = false) {
 
-  var video_id = get_video_id(video);
+  var videoId = getVideoId(video);
 
-  ytdl.getInfo("https://www.youtube.com/watch?v=" + video_id, (error, info) => {
+  ytdl.getInfo("https://www.youtube.com/watch?v=" + videoId, (error, info) => {
     if(error) {
-      message.reply("The requested video (" + video_id + ") does not exist or cannot be played.");
-      console.log("Error (" + video_id + "): " + error);
+      msg.reply("The requested video (" + videoId + ") does not exist or cannot be played.");
     } else {
-      queue.push({title: info["title"], id: video_id, user: message.author.username});
+      queue[msg.guild.id].push({title: info["title"], id: videoId, user: msg.author.username});
       if (!mute) {
-        message.reply('"' + info["title"] + '" has been added to the queue.');
+        msg.reply('"' + info["title"] + '" has been added to the queue.');
       }
-      if(!paused && !is_bot_playing() && queue.length === 1) {
-        play_next_song();
+      if(!isPaused && !isBotPlaying(msg.guild.id) && queue[msg.guild.id].length === 1) {
+        playNextSong(msg);
       }
     }
   });
 }
 
-function get_video_id(string) {
+function getVideoId(string) {
   var regex = /(?:\?v=|&v=|youtu\.be\/)(.*?)(?:\?|&|$)/;
   var matches = string.match(regex);
 
@@ -146,43 +204,38 @@ function get_video_id(string) {
   }
 }
 
-function play_next_song() {
-  if(is_queue_empty()) {
-    text_channel.sendMessage("The queue is empty!");
+function playNextSong(msg) {
+  if(isQueueEmpty(msg.guild.id)) {
+    channelToSendInfo[msg.guild.id].sendmsg("The queue is empty!");
   }
 
-  var video_id = queue[0]["id"];
-  var title = queue[0]["title"];
-  var user = queue[0]["user"];
+  var videoId = queue[msg.guild.id][0]["id"];
+  var title = queue[msg.guild.id][0]["title"];
+  var user = queue[msg.guild.id][0]["user"];
 
-  now_playing_data["title"] = title;
-  now_playing_data["user"] = user;
+  channelToSendInfo.send('Now playing: "' + title + '" (requested by ' + user + ')');
 
-  if(inform_np) {
-    text_channel.send('Now playing: "' + title + '" (requested by ' + user + ')');
-  }
+  var audio_stream = ytdl("https://www.youtube.com/watch?v=" + videoId,{filter: 'audioonly'});
+  musicStream[msg.guild.id] = voiceConnection[msg.guild.id].playStream(audio_stream,{seek: 0, volume: (actualVolume[msg.guild.id]/100)});
 
-  var audio_stream = ytdl("https://www.youtube.com/watch?v=" + video_id,{filter: 'audioonly'});
-  voice_handler = voice_connection.playStream(audio_stream,{seek: 0, volume: (DEFAULT_VOLUME/100)});
-
-  voice_handler.once("end", reason => {
-    voice_handler = null;
-    console.log("Alola");
-    if(!paused && !is_queue_empty()) {
-      play_next_song();
+  musicStream[msg.guild.id].once("end", reason => {
+    musicStream[msg.guild.id] = null;
+    if(!isPaused[msg.guild.id] && !isQueueEmpty(msg.guild.id)) {
+      playNextSong(msg);
+    }else if(isQueueEmpty(msg.guild.id)){
+      channelToSendInfo[msg.guild.id].send("There are no more songs in the queue.");
     }
   });
 
-  queue.splice(0,1);
+  queue[msg.guild.id].splice(0,1);
 }
 
-function is_bot_playing() {
-  return voice_handler !== null;
+function isBotPlaying(guildId) {
+  return musicStream[guildId] !== null;
 }
 
-function is_queue_empty() {
-  return queue.length === 0;
+function isQueueEmpty(guildId) {
+  return queue[guildId].length === 0;
 }
-
 
 exports.Commands = Commands
